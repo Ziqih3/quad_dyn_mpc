5c184066aeaca61d3230be5f54934eb0baa932aa
Ziqi Hu <gavin.ziqi@gmail.com>
2023-02-18 18:31:51 -0500

    dynamic update

--- plot_mpc_states.m
+++ plot_mpc_states.m
@@ -5,14 +5,13 @@ plot_time = size(xx1, 3);
 start_time = 1;
 
 
-titles = ["vx", "vy", "vz", "Roll", "Pitch", "Yaw", "Roll_dot","pitch_dot", "yaw_dot"...
-    , "Roll_dot_last","pitch_dot_last", "yaw_dot_last",'last_thrust'];
+titles = ["vx", "vy", "vz", "Roll", "Pitch", "Yaw", "Roll_dot","pitch_dot", "yaw_dot"];
 %"tilt_angle",
 
-xx = [xx(1:3,:);rad2deg(xx(4:12,:));xx(13,:)];
+
 figure
-for k = 1:12
-    subplot(3, 4, k)
+for k = 1:9
+    subplot(3, 3, k)
     plot(xx(k, :), 'r', 'LineWidth',3)
     hold on
 %     for i = start_time:plot_time
